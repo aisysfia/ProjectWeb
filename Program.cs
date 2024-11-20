85dc3c98d3043667a503bde2559d88735f4b4fb7
sofia <nur_24002949@utp.edu.my>
2024-11-20 21:41:41 +0800

    NewBlazorApp

--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NewBlazorApp;
+using ProjectPackage;
 
-namespace ProjectPackage
-{
-    internal class Program
-    {
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<FireStoreManager>();
 
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
+await builder.Build().RunAsync();
